12c4367478a2502390674c40740bac4f47d5a522
iamrazzakk <rakibt23p@gmail.com>
2025-09-22 18:59:31 +0600

    hi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "http://146.190.126.8",
+    host: "146.190.126.8",
     port: 3002,
   },
 });
